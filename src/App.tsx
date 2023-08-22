e51131900e054fffd9163b10d736af6872228654
CrimsonLand <totonindu60320@gmail.com>
2023-08-22 23:21:04 +0200

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { ShowAge } from "./Components/ShowAge";
 function App() {
     return (
         <div className="flex h-full w-full items-center justify-center bg-Off-white">
-            <div className="h-[60vw ] w-[37%] min-w-[90%] rounded-2xl rounded-ee-[140px] bg-White p-8 xs:h-[60%] xs:min-h-[430px] xs:min-w-[560px]">
+            <div className="w-[37%] min-w-[90%] rounded-2xl rounded-ee-[140px] bg-White p-8 xs:h-[50%] xs:min-h-[430px] xs:min-w-[560px]">
                 <InputAge />
                 <ShowAge />
             </div>
